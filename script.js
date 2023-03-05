bc6f334a68fcd22856d4394ac44658ea1daffe40
its-pc <pujitachoudhary02@gmail.com>
2023-03-06 01:03:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log(document.body);
+//  
+
+
+// var a= document.getElementById("content1");
+// var b= a.getElementsByTagName("p");
+// console.log(b);
+
+// var a= document.getElementsByClassName("content2");
+// console.log(a)
+// var b= a[0].getElementsByTagName("p");
+// console.log(b[0].innerText)
+
+// b[0].innerText="fbsdjfadjfhdsjfadsbvvjdhfkdshfjdsbvmsckjavsjdbfvdsjvsd";
+// b[0].style.color="yellow"
